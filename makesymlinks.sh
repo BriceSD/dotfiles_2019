ade2009f514c9bc593061e0e1f172a929ae63899
SAUNIER DEBES Brice <brice.saunier@gmail.com>
2015-05-30 14:29:06 +0200

    Added i3

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files=".xkb .fonts .IntelliJIdea14 .nvim .tmux .tmuxinator .bash_aliases .bashrc .nvimrc .tmux.conf .vimperatorrc .vimrc.jeop .zshrc .oh-my-zsh"    # list of files/folders to symlink in homedir
+files=".xkb .fonts .i3 .IntelliJIdea14 .nvim .tmux .tmuxinator .bash_aliases .bashrc .nvimrc .tmux.conf .vimperatorrc .vimrc.jeop .zshrc .oh-my-zsh"    # list of files/folders to symlink in homedir
 
 ##########
 
