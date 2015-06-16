2a47bc8c177239f5af291b155ab441d738c528a3
SAUNIER DEBES Brice <brice.saunier@gmail.com>
2015-06-17 00:25:54 +0200

    Added ctrl to ctrl+x with xcape.

--- .xkb/jeopPETMxCape.sh
+++ .xkb/jeopPETMxCape.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 killall xcape 2> /dev/null
-${HOME}/.xkb/xcape/xcape -t '200' -e 'Shift_L=Escape;Shift_R=Escape;ISO_Level5_Shift=Tab;Super_L=space;Alt_L=Return'
+${HOME}/.xkb/xcape/xcape -t '200' -e 'Shift_L=Escape;Shift_R=Escape;ISO_Level5_Shift=Tab;Super_L=space;Alt_L=Return;Control_R=Control_R|x;Control_L=Control_L|x'
